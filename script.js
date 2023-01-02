2844dfcb1f97aecf0830b29b4cf274c4d415b2b6
vishusharmaX <sharmavishwajeet04@gmail.com>
2023-01-02 23:20:27 +0530

    initial

---
+++ script.js
@@ -0,0 +1,41 @@
+
+
+const jokes = document.querySelector('#joke');
+const jokebtn = document.querySelector('#jokebtn');
+
+
+const generateJokes =()=>{
+    const setHeader ={
+        headers : {
+            Accept : 'application/json'
+        }
+    }
+    fetch('https://icanhazdadjoke.com',setHeader)
+    .then((res)=> res.json())
+    .then((data)=>{
+        jokes.innerHTML= data.joke;
+    }).catch((error)=>{
+        console.log(error);
+    })
+
+}
+
+jokebtn.addEventListener('click',generateJokes);
+generateJokes();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
